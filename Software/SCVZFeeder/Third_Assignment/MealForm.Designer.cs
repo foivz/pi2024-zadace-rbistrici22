f391c2c67267727045eb0260588fc8df75f7ae04
rbistrici22 <rbistrici22@foi.hr>
2024-06-02 16:29:04 +0200

    Implementirana mogućnost pretraživanja na MealForm.

--- Software/SCVZFeeder/Third_Assignment/MealForm.Designer.cs
+++ Software/SCVZFeeder/Third_Assignment/MealForm.Designer.cs
@@ -3,87 +3,71 @@
     partial class MainForm
     {
         private System.ComponentModel.IContainer components = null;
-        private PI2324_rbistrici22_DBDataSet pI2324_rbistrici22_DBDataSet;
         private System.Windows.Forms.BindingSource mealsBindingSource;
-        private PI2324_rbistrici22_DBDataSetTableAdapters.MealsTableAdapter mealsTableAdapter;
-        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn ingredientsDataGridViewTextBoxColumn;
+        private System.Windows.Forms.TextBox searchTextBox;
 
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
             this.mealsBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.pI2324_rbistrici22_DBDataSet = new Third_Assignment.PI2324_rbistrici22_DBDataSet();
-            this.mealsTableAdapter = new Third_Assignment.PI2324_rbistrici22_DBDataSetTableAdapters.MealsTableAdapter();
+            this.searchTextBox = new System.Windows.Forms.TextBox();
             this.ReviewBtn = new System.Windows.Forms.Button();
             this.dvgmeal = new System.Windows.Forms.DataGridView();
             this.ViewBtn = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.mealsBindingSource)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pI2324_rbistrici22_DBDataSet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dvgmeal)).BeginInit();
             this.SuspendLayout();
-            // 
-            // mealsBindingSource
-            // 
-            this.mealsBindingSource.DataMember = "Meals";
-            this.mealsBindingSource.DataSource = this.pI2324_rbistrici22_DBDataSet;
-            // 
-            // pI2324_rbistrici22_DBDataSet
-            // 
-            this.pI2324_rbistrici22_DBDataSet.DataSetName = "PI2324_rbistrici22_DBDataSet";
-            this.pI2324_rbistrici22_DBDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
-            // 
-            // mealsTableAdapter
-            // 
-            this.mealsTableAdapter.ClearBeforeFill = true;
-            // 
+
+            // searchTextBox
+            this.searchTextBox.Location = new System.Drawing.Point(12, 12);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(200, 22);
+            this.searchTextBox.TabIndex = 0;
+            this.searchTextBox.TextChanged += new System.EventHandler(this.searchTextBox_TextChanged);
+
             // ReviewBtn
-            // 
             this.ReviewBtn.Location = new System.Drawing.Point(12, 386);
             this.ReviewBtn.Name = "ReviewBtn";
             this.ReviewBtn.Size = new System.Drawing.Size(100, 52);
-            this.ReviewBtn.TabIndex = 1;
+            this.ReviewBtn.TabIndex = 2;
             this.ReviewBtn.Text = "Review";
             this.ReviewBtn.UseVisualStyleBackColor = true;
             this.ReviewBtn.Click += new System.EventHandler(this.ReviewBtn_Click);
-            // 
+
             // dvgmeal
-            // 
             this.dvgmeal.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dvgmeal.Location = new System.Drawing.Point(12, 21);
+            this.dvgmeal.Location = new System.Drawing.Point(12, 50);
             this.dvgmeal.Name = "dvgmeal";
             this.dvgmeal.RowHeadersWidth = 51;
             this.dvgmeal.RowTemplate.Height = 24;
-            this.dvgmeal.Size = new System.Drawing.Size(1292, 359);
-            this.dvgmeal.TabIndex = 2;
-            this.dvgmeal.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
-            // 
+            this.dvgmeal.Size = new System.Drawing.Size(1292, 330);
+            this.dvgmeal.TabIndex = 3;
+            this.dvgmeal.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvmeal_CellContentClick);
+
             // ViewBtn
-            // 
             this.ViewBtn.Location = new System.Drawing.Point(135, 386);
             this.ViewBtn.Name = "ViewBtn";
             this.ViewBtn.Size = new System.Drawing.Size(97, 52);
-            this.ViewBtn.TabIndex = 3;
+            this.ViewBtn.TabIndex = 4;
             this.ViewBtn.Text = "View Review";
             this.ViewBtn.UseVisualStyleBackColor = true;
             this.ViewBtn.Click += new System.EventHandler(this.ViewBtn_Click);
-            // 
+
             // MainForm
-            // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1316, 450);
             this.Controls.Add(this.ViewBtn);
             this.Controls.Add(this.dvgmeal);
             this.Controls.Add(this.ReviewBtn);
+            this.Controls.Add(this.searchTextBox);
             this.Name = "MainForm";
             this.Text = "MealForm";
-            this.Load += new System.EventHandler(this.aMealForm_Load);
+            this.Load += new System.EventHandler(this.MainForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.mealsBindingSource)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pI2324_rbistrici22_DBDataSet)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dvgmeal)).EndInit();
             this.ResumeLayout(false);
-
+            this.PerformLayout();
         }
 
         private System.Windows.Forms.Button ReviewBtn;
--- Software/SCVZFeeder/Third_Assignment/MealForm.cs
+++ Software/SCVZFeeder/Third_Assignment/MealForm.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Windows.Forms;
 using Third_Assignment.Models;
 using Third_Assignment.Repositories;
@@ -33,14 +34,19 @@ namespace Third_Assignment
             }
         }
 
-        private void aMealForm_Load(object sender, EventArgs e)
+        private void searchTextBox_TextChanged(object sender, EventArgs e)
         {
-            // Ovdje dodajte kod za obradu događaja
-        }
+            string searchText = searchTextBox.Text.Trim().ToLower();
+            List<Meal> meals = MealRepository.GetMeals();
 
-        private void dgvmeal_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-            // Ovdje dodajte kod za obradu događaja
+            var filteredMeals = meals.Where(meal =>
+                meal.MealID.ToString().Contains(searchText) ||
+                meal.Name.ToLower().Contains(searchText) ||
+                meal.Ingredients.ToLower().Contains(searchText) ||
+                meal.Description.ToLower().Contains(searchText)).ToList();
+
+            dvgmeal.DataSource = filteredMeals;
+            dvgmeal.Refresh();
         }
 
         private void ReviewBtn_Click(object sender, EventArgs e)
@@ -50,7 +56,6 @@ namespace Third_Assignment
                 int rowIndex = dvgmeal.SelectedCells[0].RowIndex;
                 DataGridViewRow selectedRow = dvgmeal.Rows[rowIndex];
 
-                // Pretpostavimo da je MealID u prvom stupcu
                 int mealID = Convert.ToInt32(selectedRow.Cells["MealID"].Value);
 
                 this.Hide();
@@ -64,22 +69,6 @@ namespace Third_Assignment
             }
         }
 
-        private void FrmMeals_Load(object sender, EventArgs e)
-        {
-            ShowMeals();
-        }
-
-        private void ShowMeals()
-        {
-            List<Meal> meals = MealRepository.GetMeals();
-            dvgmeal.DataSource = meals;
-        }
-
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-
-        }
-
         private void ViewBtn_Click(object sender, EventArgs e)
         {
             if (dvgmeal.SelectedCells.Count > 0)
@@ -87,7 +76,6 @@ namespace Third_Assignment
                 int rowIndex = dvgmeal.SelectedCells[0].RowIndex;
                 DataGridViewRow selectedRow = dvgmeal.Rows[rowIndex];
 
-                // Pretpostavimo da je MealID u prvom stupcu
                 int mealID = Convert.ToInt32(selectedRow.Cells["MealID"].Value);
 
                 this.Hide();
@@ -101,5 +89,10 @@ namespace Third_Assignment
             }
         }
 
+        // Dodajte praznu metodu dgvmeal_CellContentClick da izbjegnete pogreške prilikom kompajliranja
+        private void dgvmeal_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // Ovdje možete dodati kod za obradu događaja ako je potrebno
+        }
     }
 }
